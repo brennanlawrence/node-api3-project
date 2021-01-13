e45741e12bc008be7c2274b3852c3b9f7d51c7d2
brennanlawrence <brennanlawrence2002@gmail.com>
2021-01-13 09:28:37 -0700

    Finished project

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,25 +1,71 @@
-const express = require('express');
+const express = require("express");
+const Posts = require("./posts-model");
+const Middleware = require("../middleware/middleware");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // do your magic!
+  Posts.get()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Whoops, something went terribly wrong on our side... " + err,
+      });
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", Middleware.validatePostId, (req, res) => {
   // do your magic!
   // this needs a middleware to verify post id
+  const { id } = req.params;
+  Posts.getById(id)
+    .then((post) => {
+      res.status(200).json(post);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Whoops, something went terribly wrong on our side... " + err,
+      });
+    });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", Middleware.validatePostId, (req, res) => {
   // do your magic!
   // this needs a middleware to verify post id
+  const { id } = req.params;
+  Posts.remove(id)
+    .then((numOfPostsRemoved) => {
+      res
+        .status(200)
+        .json({ message: `Post with id: ${id}, successfully removed.` });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Whoops, something went terribly wrong on our side... " + err,
+      });
+    });
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", Middleware.validatePostId, Middleware.validatePost, (req, res) => {
   // do your magic!
   // this needs a middleware to verify post id
   // and another middleware to check that the request body is valid
+  const { id } = req.params;
+  const changes = req.body;
+  Posts.insert(id, changes).then((numOfPostsRemoved) => {
+    res
+      .status(200)
+      .json({ message: `Post with id: ${id}, successfully updated.` });
+  })
+  .catch((err) => {
+    res.status(500).json({
+      message: "Whoops, something went terribly wrong on our side... " + err,
+    });
+  });
 });
 
 // do not forget to export the router
+module.exports = router;
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,41 +1,113 @@
-const express = require('express');
+const express = require("express");
+const Users = require("./users-model");
+const Middleware = require("../middleware/middleware");
+
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", Middleware.validateUser, (req, res) => {
   // do your magic!
   // this needs a middleware to check that the request body is valid
+  Users.insert(req.body).then((user) => {
+    res.status(200).json(user);
+  })
+  .catch((err) => {
+    res.status(500).json({ message: "Something went terribly wrong..." + err });
+  });
 });
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // do your magic!
+  Users.get()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Something went terribly wrong..." });
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", Middleware.validateUserId, (req, res) => {
+  const { id } = req.params;
+  Users.getById(id)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({
+          message:
+            "Something went terribly wrong... We failed to get the user you asked for.",
+        });
+    });
   // do your magic!
   // this needs a middleware to verify user id
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", Middleware.validateUserId, (req, res) => {
   // do your magic!
   // this needs a middleware to verify user id
+  const { id } = req.params;
+  Users.remove(id)
+    .then((numOfRecodsDeleted) => {
+      res.status(200).json({ message: `User with id: ${id}, deleted.` });
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({
+          message:
+            "Something went terribly wrong... We failed to delete the user you asked for.",
+        });
+    });
 });
 
-router.put('/:id', (req, res) => {
+router.put(
+  "/:id",
+  Middleware.validateUserId,
+  Middleware.validateUser,
+  (req, res) => {
+    // do your magic!
+    // this needs a middleware to verify user id
+    // and another middleware to check that the request body is valid
+    const { id } = req.params;
+    const changes = req.body;
+
+    Users.update(id, changes)
+      .then((numOfRecodsUpdated) => {
+        res.status(200).json({ message: `User with id: ${id}, updated.` });
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .json({
+            message:
+              "Something went terribly wrong... We failed to update the user you asked for.",
+          });
+      });
+  }
+);
+
+//?? Not sure this is supposed to be here, it doesn't match the data structure or the helpers. ??
+router.post("/:id/posts", Middleware.validateUserId, (req, res) => {
   // do your magic!
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
 
-router.post('/:id/posts', (req, res) => {
+router.get("/:id/posts", Middleware.validateUserId, (req, res) => {
   // do your magic!
   // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
-});
-
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-  // this needs a middleware to verify user id
+  const { id } = req.params;
+  Users.getUserPosts(id).then((usersPosts) => {
+    res.status(200).json(usersPosts);
+  })
+  .catch((err) => {
+    res.status(500).json({ message: "Something went terribly wrong... " + err });
+  });
 });
 
 // do not forget to export the router
+
+module.exports = router;
